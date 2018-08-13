929038b978a93f28fd784d079dc2da30f4eb241a
Rodrigo Villardi Cortezi <rodrigovcortezi@gmail.com>
2018-08-13 00:24:19 -0300

    Cria o dropdown da barra de navegação em mobile.

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,7 +1,23 @@
+var buttonActive = false;
 const menuButton = document.getElementById('menu-icon');
 menuButton.addEventListener('click', function() {
     const header = document.getElementsByTagName('header')[0];
     const nav = document.getElementsByTagName('nav')[0];
-    header.style.height = '100%';
-    nav.style.height = '100%';
+    if(buttonActive) {
+	nav.style.height = '60px'
+	header.style.height = '60px';
+    } else {
+	header.style.height = '100%';
+	nav.style.height = '100%';
+    }
+
+    buttonActive = !buttonActive;
 });
+
+window.onresize = function() {
+    const header = document.getElementsByTagName('header')[0];
+    const nav = document.getElementsByTagName('nav')[0];
+    header.style.height = '';
+    nav.style.height = '';
+    buttonActive = false;
+}
